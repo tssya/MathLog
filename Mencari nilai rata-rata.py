e5e615bc4765279db1bdb8fd80dd219148f196ef
Tasya <auliadh.tasya@gmail.com>
2025-06-18 23:54:07 +0700

    cari nilai rata-rata

---
+++ Mencari nilai rata-rata.py
@@ -0,0 +1,16 @@
+nama = input("Masukkan nama anda: ")
+npm = input("Masukkan NPM anda: ")
+
+print("List nilai mata kuliah:")
+
+dpp = float(input("Dasar-Dasar Pemrograman: "))
+si = float(input("Sistem informasi: "))
+bd = float(input("Basis Data: "))
+m = float(input("Multimedia: "))
+k = float(input("Kalkulus: "))
+an = float(input("Analisis Numerik: "))
+
+nilai = dpp + si + bd + m + k + an
+rata_rata = nilai / 6
+
+print("Rata-rata nilai", nama, "dengan NPM", npm, "adalah:", rata_rata)
